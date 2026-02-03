8f236b8e96e1afd21d0b1a748ea20bf7d72a4788
Tejesh Neelam <tejesh@example.com>
2026-02-03 17:50:16 +0530

    Prepare for deployment: remove proxy, set axios base URL, add vercel config

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 import './index.css';
 import App from './App';
 import { CartProvider } from './context/CartContext';
+
+// Set axios base URL
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:5001';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
